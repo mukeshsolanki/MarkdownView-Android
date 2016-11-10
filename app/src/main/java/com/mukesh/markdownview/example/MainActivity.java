d9be4e8496001b3f083e714a4663650ef7cd0ee6
谭瀚涛 <littlehans8@gmail.com>
2016-11-11 00:26:50 +0800

    update demo

--- app/src/main/java/com/mukesh/markdownview/example/MainActivity.java
+++ app/src/main/java/com/mukesh/markdownview/example/MainActivity.java
@@ -10,7 +10,10 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdown_view);
-    markdownView.setMarkDownText("# Hello World\nThis is a simple markdown");
+    markdownView.setMarkDownText("# Hello World\nThis is a simple markdown\n"
+        + "https://github.com/mukeshsolanki/MarkdownView-Android/");
     //markdownView.loadMarkdownFromAssets("README.md");
+    markdownView.setOpenUrlInBrowser(true); // default false
+
   }
 }
